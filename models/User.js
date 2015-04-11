f112e8a242faee195db4aa84fcd784272d6cf838
Natahlie <natahliebeavers@gmail.com>
2015-04-11 11:12:15 -0500

    started removing code we don't need

--- models/User.js
+++ models/User.js
@@ -6,20 +6,13 @@ var userSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String,
 
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
   tokens: Array,
 
   profile: {
     name: { type: String, default: '' },
-    gender: { type: String, default: '' },
-    location: { type: String, default: '' },
-    website: { type: String, default: '' },
-    picture: { type: String, default: '' }
+    phoneNumber: { type: String, default: '' },
+    address: { type: String, default: '' },
+    email: { type: String, default: '' }
   },
 
   resetPasswordToken: String,
