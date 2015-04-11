60e0a55b789e230b92879c0831506e4a976cce16
Natahlie <natahliebeavers@gmail.com>
2015-04-11 11:31:22 -0500

    temp_commit

--- controllers/user.js
+++ controllers/user.js
@@ -121,9 +121,8 @@ exports.postUpdateProfile = function(req, res, next) {
     if (err) return next(err);
     user.email = req.body.email || '';
     user.profile.name = req.body.name || '';
-    user.profile.gender = req.body.gender || '';
-    user.profile.location = req.body.location || '';
-    user.profile.website = req.body.website || '';
+    user.profile.phoneNumber = req.body.phoneNumber || '';
+    user.profile.address = req.body.address || '';
 
     user.save(function(err) {
       if (err) return next(err);
