9b2d5e4d73d2b392799600bc6b9b7c880332514c
Natahlie <natahliebeavers@gmail.com>
2015-04-14 11:32:58 -0500

    updated todo page, started updating not logged in home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,18 @@
 /**
  * GET /
  * Home page.
+
+
  */
 exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
+  if(req.user) { 
+    res.render('home', {
+      title: 'Home'
+    });
+  }
+  else {
+    res.render('homeNotLoggedIn', {
+        title: 'Home Not Logged In'
+    });
+  }
 };
